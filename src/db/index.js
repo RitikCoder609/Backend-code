1ff98422d1aa7c6fc295a1324ed1828c3d173ff0
Ritik Kumar <dhacker401@gmail.com>
2026-05-24 12:41:03 +0530

    feat: set up Express server with middleware and MongoDB connection
    - Updated package.json to include necessary dependencies: cookie-parser, cors, dotenv, express, mongoose.
    - Created app.js to initialize Express app with CORS, cookie parsing, and JSON body parsing.
    - Added constants.js for database name configuration.
    - Implemented database connection logic in db/index.js using Mongoose.
    - Configured server startup in index.js with environment variable support.
    - Introduced ApiError and ApiResponse utility classes for standardized error and response handling.
    - Added asyncHandler utility for handling asynchronous route handlers.

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import dns from "node:dns";
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const connectDB = async () =>{
+    try{
+      const connectionInstance =  await 
+    //   mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    mongoose.connect(process.env.MONGODB_URI, { dbName: DB_NAME })
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        
+    }catch(error){
+        console.log("MONGODB connection Failed" , error);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB
