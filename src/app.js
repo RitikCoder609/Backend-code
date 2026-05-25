afd306a25bd780f9eab4d7b2e63eeea2acc16526
Ritik Kumar <dhacker401@gmail.com>
2026-05-25 12:32:44 +0530

    all setup Configured

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express();
 
+const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -12,11 +12,14 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+//localhost:8000/api/v1/users/register
 export { app };
--- src/index.js
+++ src/index.js
@@ -2,23 +2,19 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-
-
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
-.then(()=> {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
-        
-    })
-})
-.catch((err) => {
-    console.log("Mongo DB connection Failed !!!" , err);
-    
-})
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection Failed !!!", err);
+  });
 
 
 
@@ -27,24 +23,7 @@ connectDB()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 /*
 import express from "express";
 
