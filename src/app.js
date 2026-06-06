c19f6336b24ac6791e1fc7245df80cebfe6b326d
Ritik Kumar <dhacker401@gmail.com>
2026-06-06 16:17:12 +0530

    refactor: remove root route for backend status check

--- src/app.js
+++ src/app.js
@@ -13,12 +13,6 @@ app.use(
 
 
 
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
-
-
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
